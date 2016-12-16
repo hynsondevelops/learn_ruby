c6e6c104dc5459d166a8a2bff86589345e600b48
Adam Hynson <hynsondevelops@gmail.com>
2016-12-16 15:06:24 -0500

    Completed through 02_calculator

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,37 @@
 #write your code here
+
+def add(x, y)
+	return (x + y);
+end
+
+def subtract(x, y)
+	return (x - y);
+end
+
+def sum(x)
+	sum = 0;
+	for i in 0..(x.length - 1)
+		sum = sum + x.at(i);
+	end
+	return sum;
+end
+
+def multiply(x)
+	product = 1;
+	for i in 0..(x.length - 1)
+		product = product * x.at(i);
+	end
+	return product;
+end
+
+def power(x, y)
+	return (x**y);
+end
+
+def factorial(x)
+	factorial = 1; 
+	for i in x.downto(2)
+		factorial *= i;
+	end
+	return factorial;
+end
