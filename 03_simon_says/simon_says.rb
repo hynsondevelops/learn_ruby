4e3b72c5b817290ef5b4aefd3712489c38c6fd89
Adam Hynson <hynsondevelops@gmail.com>
2016-12-18 17:17:03 -0500

    Finished Pig Latin

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -1,1 +1,51 @@
 #write your code here
+
+def echo(x)
+	return x;
+end
+
+def shout(x)
+	return x.upcase;
+end
+
+def repeat(word, times = 2)
+	newWord = word;
+	for x in 2..times
+		newWord += " " + word;
+	end
+	return newWord;
+end
+
+def start_of_word(word, letters)
+	start = "";
+	for i in 0..(letters - 1)
+		start += word[i];
+	end
+	return (start);
+end
+
+def first_word(word)
+	letter = '';
+	firstWord = "";
+	i = 0;
+	while (letter != ' ')
+		firstWord += word[i];
+		letter = word[i + 1];
+		i += 1;
+	end
+	return firstWord;
+end
+
+def titleize(title)
+	prepositionList = ["and", "or", "the", "a", "an", "if", "over"];
+	wordList = title.split;
+	wordList[0] = wordList[0].capitalize;
+	for i in 1..(wordList.size - 1)
+		if (prepositionList.include?(wordList[i]))
+		else
+			wordList[i] = wordList[i].capitalize;
+		end
+	end
+	newTitle = wordList.join(" ");
+	return newTitle;
+end
--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,1 +1,29 @@
 #write your code here
+def translate(words)
+	vowels = ['a', 'e', 'i', 'o', 'u'];
+	wordArray = words.split;
+	for j in 0..(wordArray.size - 1)
+		word = wordArray[j];
+		consonantOrVowel = false;
+		consonantCounter = 0;
+		i = 0;
+		while (!consonantOrVowel)
+			if (vowels.include?(word[i]))
+				if (word[i] == 'u' &&  word[i - 1] == 'q')
+					word += word[i];
+					consonantCounter += 1;
+				end
+				consonantOrVowel = true;
+			else
+				word += word[i];
+				consonantCounter += 1;
+			end
+			i += 1;
+		end
+		word << "ay";
+		wordArray[j] = word[(consonantCounter..-1)];
+	end
+	return wordArray.join(" ");
+end
+
+
