6c3582e6a1fd6b90940e6beea9b37b07fe763a7e
Adam Hynson <hynsondevelops@gmail.com>
2016-12-18 18:43:46 -0500

    Completed Book_Titles

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,33 @@
 class Book
+
+	def initilize()
+
+	end
+	#def initialize(title)
+	#	@title = titleize(title);
+	#end
+
+	def title=(title) 
+		@title = titleize(title);
+	end
+
+	def title
+		return @title;
+	end
+
+
+	def titleize(thisTitle)
+		prepositionList = ["in", "of", "and", "or", "the", "a", "an", "if", "over"];
+		wordList = thisTitle.split;
+		wordList[0] = wordList[0].capitalize;
+		for i in 1..(wordList.size - 1)
+			if (prepositionList.include?(wordList[i]))
+			else
+				wordList[i] = wordList[i].capitalize;
+			end
+		end
+		newTitle = wordList.join(" ");
+		return newTitle;
+	end
 # write your code here
 end
